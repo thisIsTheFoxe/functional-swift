4a5f9c8ddb03d74e4a6f29e9f154c63929ce2131
Henrik Storch <henrik_storch@hugoboss.com>
2020-08-29 11:42:35 +0200

    fix imports + add typealias

--- FuncSwift.playground/Contents.swift
+++ FuncSwift.playground/Contents.swift
@@ -1,4 +1,6 @@
-import UIKit
+import Foundation
+
+typealias FUNC = (Any, Any) -> Any
 
 func TRUE(x: Any, y: Any) -> Any {
     return x
@@ -9,14 +11,14 @@ func FALSE(x: Any, y: Any) -> Any {
 }
 
 func NOT(x: Any) -> Any {
-    guard let x = x as? (Any, Any) -> Any else { fatalError() }
+    guard let x = x as? FUNC else { fatalError() }
     return x(FALSE, TRUE)
 }
 
-(NOT(x: TRUE) as! (Any, Any) -> Any)(1,0)
+(NOT(x: TRUE) as! FUNC)(1,0)
 
 func AND(x: Any, y: Any) -> Any {
-    guard let x = x as? (Any, Any) -> Any else { fatalError() }
+    guard let x = x as? FUNC else { fatalError() }
     return x(y, FALSE)
 }
 
@@ -24,17 +26,17 @@ func AND(x: Any, y: Any) -> Any {
 
 
 func OR(x: Any, y: Any) -> Any {
-    guard let x = x as? (Any, Any) -> Any else { fatalError() }
+    guard let x = x as? FUNC else { fatalError() }
     return x(TRUE, y)
 }
 
 (OR(x: FALSE, y: TRUE) as! (Any, Any) -> Any)(1,0)
 
 func XOR(x: Any, y: Any) -> Any {
-    guard let x = x as? (Any, Any) -> Any else { fatalError() }
+    guard let x = x as? FUNC else { fatalError() }
     return x(NOT(x: y), y)
 }
 
-(XOR(x: TRUE, y: TRUE) as! (Any, Any) -> Any)(1,0)
+(XOR(x: TRUE, y: TRUE) as! FUNC)(1,0)
 
-(AND(x: OR(x: TRUE, y: FALSE), y: XOR(x: FALSE, y: TRUE)) as! (Any, Any) -> Any)(1,0)
+(AND(x: OR(x: TRUE, y: FALSE), y: XOR(x: FALSE, y: TRUE)) as! FUNC)(1,0)
